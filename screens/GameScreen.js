6527db110088c61c3e84cd03f12a41ad901badcb
r.danna <rafdanna28@gmail.com>
2022-12-19 12:36:32 +0100

    aggiunta icone +/- tramite import libreria Ionicons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,8 @@
 import {View, StyleSheet, Alert} from "react-native";
-import Title from "../components/ui/Title";
 import {useEffect, useState} from "react";
+import {Ionicons} from '@expo/vector-icons'
+
+import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
@@ -57,10 +59,14 @@ function GameScreen({userNumber, onGameOver}){
                 <InstructionText mystyle = {styles.instructionText}> Higher or lower?</InstructionText>
                 <View style = {styles.buttonsContainer}>
                     <View style = {styles.buttonContainer}>
-                        <PrimaryButton onPressButton = {nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+                        <PrimaryButton onPressButton = {nextGuessHandler.bind(this, 'lower')}>
+                            <Ionicons name = "md-remove" size = {24} color = "white"/>
+                        </PrimaryButton>
                     </View>
                     <View style = {styles.buttonContainer}>
-                        <PrimaryButton onPressButton = {nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+                        <PrimaryButton onPressButton = {nextGuessHandler.bind(this, 'greater')}>
+                            <Ionicons name = "md-add" size = {24} color = "white"/>
+                        </PrimaryButton>
                     </View>
                 </View>
             </Card>
