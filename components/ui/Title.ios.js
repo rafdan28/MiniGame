7930bc1d4080ff3e3f6bc61798acb9e9154239c0
r.danna <rafdanna28@gmail.com>
2022-12-20 15:41:42 +0100

    aggiunti colori e componente title differenti per ios e android

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text} from "react-native";
+import {StyleSheet, Text, Platform} from "react-native";
 import Colors from "../../constants/colors";
 function Title({children}){
     return(
@@ -15,7 +15,8 @@ const styles = StyleSheet.create({
         // fontWeight: 'bold',
         color: Colors.accent500,
         textAlign: 'center',
-        borderWidth: 2,
+        // borderWidth: Platform.OS === 'android' ? 2 : 0,
+        borderWidth: Platform.select({ios: 0, android: 2}),
         borderColor: Colors.accent500,
         padding: 12,
         maxWidth: '80%',
---
+++ constants/colors.android.js
@@ -0,0 +1,9 @@
+const Colors = {
+    primary500: '#cb086a',
+    primary600: '#81184e',
+    primary700: '#56253d',
+    primary800: '#b6005d',
+    accent500: '#f5ba00',
+}
+
+export default Colors;
