ae473159731e3d6e22fd8c8ab6813ff88da98658
r.danna <rafdanna28@gmail.com>
2022-12-20 15:30:30 +0100

    migliorata interfaccia utente della schermata GameOverScreen.js con ratazione attiva

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,31 +1,55 @@
-import {Image, View, StyleSheet, Text, Dimensions} from "react-native";
+import {Image, View, StyleSheet, Text, useWindowDimensions, ScrollView} from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
 
 function GameOverScreen({roundsNumber, userNumber, onStartNewGame}){
+    const {width, height} = useWindowDimensions();
+
+    let imageSize = 300;
+
+    if(width < 380){
+        imageSize = 150;
+    }
+
+    if(height < 450){
+        imageSize = 100;
+    }
+
+    const imageStyle = {
+        width: imageSize,
+        height: imageSize,
+        borderRadius: imageSize / 2
+    }
+
     return(
-        <View style = {styles.rootContainer}>
-            <Title>GAME OVER!</Title>
-            <View style = {styles.imageContainer}>
-                <Image
-                    style = {styles.image }
-                    source = {require('../assets/images/success.png')}
-                />
+        <ScrollView style={styles.screen}>
+            <View style = {styles.rootContainer}>
+                <Title>GAME OVER!</Title>
+                <View style = {[styles.imageContainer, imageStyle]}>
+                    <Image
+                        style = {styles.image }
+                        source = {require('../assets/images/success.png')}
+                    />
+                </View>
+                <Text style = {styles.summaryText}>
+                    Il telefono ha indovinato in <Text style = {styles.highlight}>{roundsNumber}</Text> mosse il numero <Text style = {styles.highlight}>{userNumber}</Text>.
+                </Text>
+                <PrimaryButton onPressButton = {onStartNewGame}>Nuova Partita</PrimaryButton>
             </View>
-            <Text style = {styles.summaryText}>
-                Il telefono ha indovinato in <Text style = {styles.highlight}>{roundsNumber}</Text> mosse il numero <Text style = {styles.highlight}>{userNumber}</Text>.
-            </Text>
-            <PrimaryButton onPressButton = {onStartNewGame}>Nuova Partita</PrimaryButton>
-        </View>
+        </ScrollView>
     );
 }
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get('window').width;
+// const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
+    screen:{
+      flex: 1
+    },
+
     rootContainer: {
         flex: 1,
         padding: 24,
@@ -34,9 +58,9 @@ const styles = StyleSheet.create({
     },
 
     imageContainer: {
-        width: deviceWidth < 380 ? 150 : 300,
-        height: deviceWidth < 380 ? 150 : 300,
-        borderRadius: deviceWidth < 380 ? 75 : 150,
+        // width: deviceWidth < 380 ? 150 : 300,
+        // height: deviceWidth < 380 ? 150 : 300,
+        // borderRadius: deviceWidth < 380 ? 75 : 150,
         borderWidth: 3,
         borderColor: Colors.primary800,
         overflow: 'hidden',
